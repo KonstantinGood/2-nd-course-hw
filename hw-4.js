763669798f9f629edd4c0fd24939cee05618537f
KonstantinGood <laudanum1995@yandex.ru>
2023-07-15 19:51:12 +0300

    hw-4

---
+++ hw-4.js
@@ -0,0 +1,49 @@
+//Задание 1
+
+let m = 0;
+while (m < 2) {
+    console.log ('Привет');
+    m++
+}
+ //Задание 2
+
+ let i = 0;
+
+while (i < 6) {
+	console.log(i);
+	i++;
+}
+
+//Задание 3
+
+let n = 7;
+
+while (n < 23) {
+	console.log(n);
+	n++;
+}
+
+//Задание 4
+
+let obj = {'Коля': 200, 'Вася':300, 'Петя': 400};
+
+for(key in obj){
+    console.log([key]+ ' - Зарплата ' + obj[key] +' долларов');
+}
+
+//Задание 5
+
+let a = 1000, s= 0;
+while(a >= 50){
+    a = a/2;
+  s++;
+}
+console.log("Итераций: " + s +", получилось число " + a);
+
+
+//Задание 6
+
+for (let fridey = 7; fridey <=31; fridey = fridey+7) {
+  console.log (`Сугодня пятница ${fridey}-е число. Необходимо подготовить отчет.`)
+}
+                  
